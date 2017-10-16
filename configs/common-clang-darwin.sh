6a9340cf9937e744924b9ff5a476998145ded5a7
Nathan Toone <nathan@toonetown.com>
2017-10-16 09:19:06 -0600

    Fix visibility issue for clang compilations of boost::log

--- configs/common-clang-darwin.sh
+++ configs/common-clang-darwin.sh
@@ -4,7 +4,7 @@ cat << EOF > "${PATH_TO_BOOST_DIST}/project-config.jam"
 using clang-darwin : : ${CLANG_PATH}
   : <compileflags>-arch <compileflags>${ARCH}
     <compileflags>-m${SDK_VERSION_NAME}-version-min=${MIN_OS_VERSION}
-    <cxxflags>-std=c++11 <cxxflags>-stdlib=libc++
+    <cxxflags>-std=c++11 <cxxflags>-stdlib=libc++ <cxxflags>-fvisibility=default
     <compileflags>-fPIC <compileflags>-fembed-bitcode
     <compileflags>-isysroot <compileflags>${SDK_PATH}
     <linkflags>-arch <linkflags>${ARCH}
