b2af280c049127c3fa8733354460ccee0ac7f804
Nathan Toone <nathan@toonetown.com>
2017-10-16 12:51:24 -0600

    Build targeting windows 7 or later.

--- configs/setup-windows.bat
+++ configs/setup-windows.bat
@@ -1,3 +1,6 @@
 @echo off
-SET OPTS=architecture=x86 define=BOOST_LOG_USE_COMPILER_TLS
+SET MIN_WIN_NT_VERSION=0x0601
+SET OPTS=architecture=x86 define=WINVER=%MIN_WIN_NT_VERSION% ^
+                          define=_WIN32_WINNT=%MIN_WIN_NT_VERSION% ^
+                          define=BOOST_LOG_USE_COMPILER_TLS
 exit /B 0
