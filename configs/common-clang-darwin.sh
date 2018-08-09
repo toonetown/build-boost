3f5386b8880dfd39bb2a23abe93f0aeb0df523d3
Nathan Toone <nathan@toonetown.com>
2018-08-09 05:31:44 -0600

    Clean up project-config.jam a bit. *IMPORTANT:* This removes the explicit setting of `-fvisibility=default` because changes in the build system make it not consistent how it is applied.

--- configs/common-clang-darwin.sh
+++ configs/common-clang-darwin.sh
@@ -2,14 +2,11 @@
 
 cat << EOF > "${PATH_TO_BOOST_DIST}/project-config.jam"
 using clang-darwin : : ${CLANG_PATH}
-  : <compileflags>-arch <compileflags>${ARCH}
+  : <compileflags>-arch <compileflags>${ARCH} <linkflags>-arch <linkflags>${ARCH}
     <compileflags>-m${SDK_VERSION_NAME}-version-min=${MIN_OS_VERSION}
-    <cxxflags>-std=c++11 <cxxflags>-stdlib=libc++ <cxxflags>-fvisibility=default
-    <compileflags>-fPIC <compileflags>-fembed-bitcode
-    <compileflags>-isysroot <compileflags>${SDK_PATH}
-    <linkflags>-arch <linkflags>${ARCH}
     <linkflags>-m${SDK_VERSION_NAME}-version-min=${MIN_OS_VERSION}
-    <linkflags>-stdlib=libc++
-    <linkflags>-isysroot <linkflags>${SDK_PATH}
+    <compileflags>-isysroot <compileflags>${SDK_PATH} <linkflags>-isysroot <linkflags>${SDK_PATH}
+    <cxxflags>-std=c++11 <cxxflags>-stdlib=libc++ <linkflags>-stdlib=libc++
+    <compileflags>-fPIC <compileflags>-fembed-bitcode
   ;
 EOF
