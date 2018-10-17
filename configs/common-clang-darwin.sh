edd7b363d3722a205d7d05db6e658193204dab05
Nathan Toone <nathan@toonetown.com>
2018-10-17 08:47:32 -0600

    Allow environment variable specification of c++ version

--- configs/common-clang-darwin.sh
+++ configs/common-clang-darwin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+CXX_VERSION="${CXX_VERSION:-17}"
 
 cat << EOF > "${PATH_TO_BOOST_DIST}/project-config.jam"
 using clang-darwin : : ${CLANG_PATH}
@@ -6,7 +7,7 @@ using clang-darwin : : ${CLANG_PATH}
     <compileflags>-m${SDK_VERSION_NAME}-version-min=${MIN_OS_VERSION}
     <linkflags>-m${SDK_VERSION_NAME}-version-min=${MIN_OS_VERSION}
     <compileflags>-isysroot <compileflags>${SDK_PATH} <linkflags>-isysroot <linkflags>${SDK_PATH}
-    <cxxflags>-std=c++17 <cxxflags>-stdlib=libc++ <linkflags>-stdlib=libc++
+    <cxxflags>-std=c++${CXX_VERSION} <cxxflags>-stdlib=libc++ <linkflags>-stdlib=libc++
     <compileflags>-fPIC <compileflags>-fembed-bitcode
     <cxxflags>-fvisibility=default
   ;
--- configs/setup-windows.bat
+++ configs/setup-windows.bat
@@ -1,8 +1,11 @@
 @echo off
 SET MIN_WIN_NT_VERSION=0x0601
+IF "%CXX_VERSION%"=="" SET CXX_VERSION=17
+SET CXX_OPTS=
+IF "%CXX_VERSION%"=="17" SET CXX_OPTS=define=_SILENCE_ALL_CXX17_DEPRECATION_WARNINGS ^
+                                      cxxflags=/std:c++17
 SET OPTS=architecture=x86 define=WINVER=%MIN_WIN_NT_VERSION% ^
                           define=_WIN32_WINNT=%MIN_WIN_NT_VERSION% ^
                           define=BOOST_LOG_USE_COMPILER_TLS ^
-                          define=_SILENCE_ALL_CXX17_DEPRECATION_WARNINGS ^
-                          cxxflags=/std:c++17
+                          %CXX_OPTS%
 exit /B 0
